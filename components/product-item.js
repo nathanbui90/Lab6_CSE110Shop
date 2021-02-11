9811c37cb77f53e77a4599ea2d1859db4753af17
nathan42424 <nathanbui90@gmail.com>
2021-02-10 20:02:24 -0800

    Final Commit

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,91 @@
 // product-item.js
+const template = document.createElement('template');
+template.innerHTML = `
+<style>
+.price {
+  color: green;
+  font-size: 1.8em;
+  font-weight: bold;
+  margin: 0;
+}
+
+.product {
+  align-items: center;
+  background-color: white;
+  border-radius: 5px;
+  display: grid;
+  grid-template-areas: 
+  'image'
+  'title'
+  'price'
+  'add';
+  grid-template-rows: 67% 11% 11% 11%;
+  height: 450px;
+  filter: drop-shadow(0px 0px 6px rgb(0,0,0,0.2));
+  margin: 0 30px 30px 0;
+  padding: 10px 20px;
+  width: 200px;
+}
+
+.product > button {
+  background-color: rgb(255, 208, 0);
+  border: none;
+  border-radius: 5px;
+  color: black;
+  justify-self: center;
+  max-height: 35px;
+  padding: 8px 20px;
+  transition: 0.1s ease all;
+}
+
+.product > button:hover {
+  background-color: rgb(255, 166, 0);
+  cursor: pointer;
+  transition: 0.1s ease all;
+}
+
+.product > img {
+  align-self: center;
+  justify-self: center;
+  width: 100%;
+}
+
+.title {
+  font-size: 1.1em;
+  margin: 0;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+
+.title:hover {
+  font-size: 1.1em;
+  margin: 0;
+  white-space: wrap;
+  overflow: auto;
+  text-overflow: unset;
+}
+</style>
+<li class="product">
+<img width=200>
+<p class="title"></p>
+<p class="price"></p>
+<button onclick="addTo(event)">Add to Cart</button>
+</li>`
 
 class ProductItem extends HTMLElement {
   // TODO
+  constructor(){
+    super();
+    
+    this.attachShadow({ mode: 'open'});
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.shadowRoot.querySelector('img').src = this.getAttribute('srcImg');
+    this.shadowRoot.querySelector('img').alt = this.getAttribute('altImg');
+    this.shadowRoot.querySelector('.title').innerHTML = this.getAttribute('title');
+    this.shadowRoot.querySelector('.price').innerHTML = this.getAttribute('price');
+    this.shadowRoot.querySelector('button').name = this.getAttribute('id');
+  }
 }
 
 customElements.define('product-item', ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,52 @@
 // Script.js
+var cartCount = 0;
+var cart = [];
+storage = window.localStorage;
 
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
+  if(!(localStorage.getItem("items")==true)){
+    fetch('https://fakestoreapi.com/products')
+    .then(response => response.json())
+    .then(data => localStorage.setItem("items", JSON.stringify(data)));
+  }
+
+  let items = localStorage.getItem("items");
+  let previousCart = localStorage.getItem("prevCart")
+  
+  JSON.parse(items).forEach(productItem);
+  
+  if(previousCart){
+    JSON.parse(previousCart).forEach(resetCart);
+  }
 });
+
+function productItem(item){
+  var product = `<product-item srcImg="${item.image}" altImg="${item.title}" title="${item.title}" price="${item.price}" id="${item.id}">`;
+  document.getElementById("product-list").insertAdjacentHTML('beforeend', product);
+}
+
+
+function addTo(event){
+  if(event.target.innerHTML == "Add to Cart"){
+    event.target.innerHTML = "Remove from Cart";
+    cartCount++;
+    document.getElementById("cart-count").innerHTML = cartCount;
+    cart.push(event.target.name);
+    localStorage.setItem("prevCart", JSON.stringify(cart));
+  }
+  else{
+    event.target.innerHTML = "Add to Cart";
+    cartCount--;
+    document.getElementById("cart-count").innerHTML = cartCount;
+    cart.splice(cart.indexOf(event.target.name), 1);
+    localStorage.setItem("prevCart", JSON.stringify(cart));
+  }
+}
+
+function resetCart(item){
+  cartCount++;
+  document.getElementById("cart-count").innerHTML = cartCount;
+  cart.push(item);
+  document.getElementById(item).shadowRoot.querySelector('button').innerHTML = "Remove from Cart";
+}
